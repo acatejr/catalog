f3ef80335155728838cec4407c2b2bdee93930d1
acatejr <acatejr@gmail.com>
2024-08-16 16:09:36 -0700

    Added some mix web scraping tools

--- crawlers/mix.exs
+++ crawlers/mix.exs
@@ -23,6 +23,9 @@ defmodule Crawlers.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+
+      {:crawly, "~> 0.17.2"},
+      {:floki, "~> 0.33.0"}
     ]
   end
 end
