0d952b909e63593bc66d88134210b611b5c7401f
acatejr <acatejr@gmail.com>
2024-09-04 13:17:11 -0700

    Init commit on branch

--- main/catalog/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from django.test import Client
-
-
-def test_landing_page():
-    """Test that landing page is redirected."""
-    c = Client()
-    response = c.get("/")
-    assert response.status_code in [301, 302, 308]
