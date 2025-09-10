8d25ecaeb9dec6f72c5cd5c88ca83793dddc54f7
acatejr@gmail.com <acatejr@gmail.com>
2025-09-10 15:36:41 -0700

    Base level client running

---
+++ slclient/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# st.title("🎈 My new app")
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
+
+st.title("Data Catalog")
+st.write("Welcome to the Data Catalog app!")
