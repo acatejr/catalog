cd29e26d539f36391f528ffae2edf8951ab003b4
acatejr@gmail.com <acatejr@gmail.com>
2025-09-19 10:44:35 -0700

    Changed timeout in slclient api requests

--- slclient/app.py
+++ slclient/app.py
@@ -37,7 +37,7 @@ if question := st.chat_input("Ask me anything about the catalog."):
                 response = httpx.get(
                     query_url + question,
                     headers={"X-API-KEY": X_API_KEY},
-                    timeout=30.0
+                    timeout=90.0
                 )
 
                 if response.status_code == 200:
